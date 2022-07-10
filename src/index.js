e0e02d229f35a55d21700d57ba70971afdc992d2
Rafa <arruda.rafa.ra@gmail.com>
2022-07-10 15:02:30 -0300

    rendering project index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./styles.css";
-
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
